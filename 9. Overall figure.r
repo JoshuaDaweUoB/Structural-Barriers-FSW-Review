0678c9ce70d6b346a779fb43ef99a96aed936244
Joshua <joshua.dawe@bristol.ac.uk>
2026-07-03 16:58:37 +0100

    .tiff for overall plots

--- 9. Overall figure.r
+++ 9. Overall figure.r
@@ -1,4 +1,3 @@
-
 # load packages 
 pacman::p_load("meta", "metafor", "readxl", "openxlsx", "tidyverse", "kableExtra", "robumeta", "clubSandwich") 
 
@@ -33,7 +32,7 @@ summary_hiv_violence <- metagen(TE = effect_ln,
   print(summary(summary_hiv_violence))
   
   # save forest plot
-  png(filename = "Plots/overall plots/violence_all_analyses.png", width = 30, height = 40, units = "cm", res = 600)
+  tiff(filename = "Plots/overall plots/violence_all_analyses.tiff", width = 30, height = 40, units = "cm", res = 600)
   forest(summary_hiv_violence, 
          sortvar = outcome,
          xlim = c(0.2, 4),             
@@ -75,7 +74,7 @@ fsw_data_overall_ever$i2 <- round(fsw_data_overall_ever$i2 * 100, 0)
   print(summary(summary_hiv_violence_ever))
   
   # save forest plot
-  png(filename = "Plots/overall plots/violence_all_analyses_ever.png", width = 30, height = 28, units = "cm", res = 600)
+  tiff(filename = "Plots/overall plots/violence_all_analyses_ever.tiff", width = 30, height = 28, units = "cm", res = 600)
   forest(summary_hiv_violence_ever, 
          sortvar = outcome,
          xlim = c(0.2, 4),             
@@ -117,7 +116,7 @@ fsw_data_overall_recent$i2 <- round(fsw_data_overall_recent$i2 * 100, 0)
   print(summary(summary_hiv_violence_rec))
   
   # save forest plot
-  png(filename = "Plots/overall plots/violence_all_analyses_recent.png", width = 30, height = 28, units = "cm", res = 600)
+  tiff(filename = "Plots/overall plots/violence_all_analyses_recent.tiff", width = 30, height = 28, units = "cm", res = 600)
   forest(summary_hiv_violence_rec, 
          sortvar = outcome,
          xlim = c(0.2, 4),             
